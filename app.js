975207ed29db7db4331c2a16ae01830ef16a6cbb
ifnavneetrajput <navneetrajput404@gmail.com>
2025-01-12 20:46:29 +0530

    initalise the repo upto episode 4

---
+++ app.js
@@ -0,0 +1,15 @@
+const { calculateSum } = require("./calculate/index.js")
+
+require("./xyz.js")
+const {multiply} = require('./calculate/index.js')
+
+
+const name = "navneet Rajput"
+
+const a = 10 
+const b = 20
+calculateSum(a,b)
+console.log(name)
+multiply(a,b)
+// console.log(a + b)
+//console.log(sum)
